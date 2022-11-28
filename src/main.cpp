a1855d7cb130e82b1c2b1f7a616972d7be515580
Braden_Nigg <bnigg2@illinois.edu>
2022-11-28 16:42:12 +0000

    Simple Data Parse Test Case

--- src/main.cpp
+++ src/main.cpp
@@ -15,14 +15,4 @@ int main(int argc, char* argv[]) {
 
     vector<vector<string>> airports = fileToV2D("../data/airports.txt");
     vector<vector<string>> routes = fileToV2D("../data/routes.txt");
-<<<<<<< HEAD
-=======
-
-    for(unsigned i =0; i < airports.size(); i++){
-        for(unsigned j = 0; j < airports[i].size(); j++){
-            cout<<airports[i][j];
-        }
-        cout<<endl;
-    }
->>>>>>> f47998f7ea31d86d3d7276c8c83d117ba6f6001a
 }
