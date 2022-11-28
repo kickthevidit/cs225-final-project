3fb9168a831c649d5b91f9fdb433f545c344d379
Braden Nigg <112449185+bnigg2@users.noreply.github.com>
2022-11-28 11:06:31 -0600

    Delete build directory

--- build/test_tests-b12d07c.cmake
+++
@@ -1,3 +0,0 @@
-add_test( [==[file_to_V2D Public Simple Small Test 1]==] /workspaces/cs225/final_project/cs225-final-project/build/test [==[file_to_V2D Public Simple Small Test 1]==]  )
-set_tests_properties( [==[file_to_V2D Public Simple Small Test 1]==] PROPERTIES WORKING_DIRECTORY /workspaces/cs225/final_project/cs225-final-project/build)
-set( test_TESTS [==[file_to_V2D Public Simple Small Test 1]==])
