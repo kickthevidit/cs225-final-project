e09175f4a2adf1423000dc94f91dc9497ef2fdc5
ryanfore5 <ryforeste2003@gmail.com>
2022-11-28 06:23:06 +0000

    tests folder

--- build/test_tests-b12d07c.cmake
+++
@@ -1,3 +0,0 @@
-add_test( test /workspaces/CS225/final-project/cs225-final-project/build/test test  )
-set_tests_properties( test PROPERTIES WORKING_DIRECTORY /workspaces/CS225/final-project/cs225-final-project/build)
-set( test_TESTS test)
--- build/tests_include-b12d07c.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/workspaces/CS225/final-project/cs225-final-project/build/tests_tests-b12d07c.cmake")
-  include("/workspaces/CS225/final-project/cs225-final-project/build/tests_tests-b12d07c.cmake")
-else()
-  add_test(tests_NOT_BUILT-b12d07c tests_NOT_BUILT-b12d07c)
-endif()
