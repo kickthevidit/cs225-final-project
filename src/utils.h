5ce82bee3365d29a4746a6195b13b209077598a8
kickthevidit <vgautam2@illinois.edu>
2022-11-28 17:12:33 +0000

    moving to utils

--- src/utils.h
+++ src/utils.h
@@ -10,6 +10,15 @@
 #include <unordered_map>
 #include <utility>
 
+
+#include <list>
+#include <memory>
+
+
+typedef std::vector<std::vector<double>> AdjMatrix;
+typedef std::list<Airport*> AirportList;
+typedef std::map<int, Airport *> AirportMap;
+
 //Library from UIUC CS225 MP schedule 
 
 /**
@@ -52,3 +61,18 @@ typedef std::vector<std::vector<std::string>> V2D;
 V2D fileToV2D(const std::string& filename);
 
 std::unordered_map<std::string, std::vector<std::string>> genMap(const V2D airports, const V2D routes);
+
+
+
+
+void ProcessAirports(AirportList &airports, const V2D & airports_dataset);
+
+void createDatasets(AirportMap &airport_map, AdjMatrix &adj, const V2D &airports, const V2D& routes);
+
+AdjMatrix ProcessAdjacencyMatrix(AirportList &airports, const V2D &routes_dataset);
+
+double CalculateDist(double long1, double lat1, double long2, double lat2);
+
+void PrintAdjMatrix(const AdjMatrix &adj, unsigned range = 10);
+
+void PrintAdjMatrix(const AdjMatrix &adj, std::string file_name = "adjacency-matrix.txt", unsigned range = 10);
