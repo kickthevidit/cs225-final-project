8ab1d25ac80ab78ffd020fdd0382e78d70940a87
Braden_Nigg <bnigg2@illinois.edu>
2022-12-11 04:01:30 +0000

    Mr. Larry Page

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "airport.h"
 #include "utils.h"
+#include "PageRank.h"
 // #include "functions.h"
 
 #include <string>
@@ -18,42 +19,32 @@ int main(int argc, char* argv[]) {
     vector<vector<string>> airports = fileToV2D("../data/airports.txt");
     vector<vector<string>> routes = fileToV2D("../data/routes.txt");
 
-    cout << "Printing Airports\n";
-    for (unsigned i = 0; i < airports.size(); i++) {
-        for(unsigned j = 0; j < airports[i].size(); j++){
-            cout<<airports[i][j] << ',';
-        }
-        cout<<endl;
-    }
+    // cout << "Printing Airports\n";
+    // for (unsigned i = 0; i < airports.size(); i++) {
+    //     for(unsigned j = 0; j < airports[i].size(); j++){
+    //         cout<<airports[i][j] << ',';
+    //     }
+    //     cout<<endl;
+    // }
 
-    cout << "Printing Routes\n";
-    for (unsigned i = 0; i < routes.size(); i++) {
-        for(unsigned j = 0; j < routes[i].size(); j++){
-            cout << routes[i][j] << ',';
-        }
-        cout << endl;
-    }
+    // cout << "Printing Routes\n";
+    // for (unsigned i = 0; i < routes.size(); i++) {
+    //     for(unsigned j = 0; j < routes[i].size(); j++){
+    //         cout << routes[i][j] << ',';
+    //     }
+    //     cout << endl;
+    // }
 
     AirportMap airport_map;
+    AirportMap airport_mapIdx;
     AdjMatrix adj;
     
-    createDatasets(airport_map, adj, airports, routes);
+    createDatasets(airport_map, airport_mapIdx, adj, airports, routes);
 
     PrintAdjMatrix(adj, "adjacency-matrix-100.txt" ,100);
 
-    // // std::ifstream file("../data/airport.csv");
+    PageRank rank(adj);
+    
+    rank.print_rankVect(airport_mapIdx);
 
-    // // std::string line;
-    // // while (std::getline(file, line)) {
-    // //     std::cout << line << std::endl;
-    // // }
-
-    // // auto vec = parse(line, '\n');
-
-    // // //std::cout << line.find(, ',') << std::endl;
-
-    // // std::cout << vec.size() << std::endl;
-    // // for (auto s : vec) {
-    // //     std::cout << s << std::endl;
-    // // }
 }
