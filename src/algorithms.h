666fcff36b6c3f00f58fe32ff80a7f52a17e0aa1
kickthevidit <vgautam2@illinois.edu>
2022-12-13 04:47:40 +0000

    Cleaned and commented algorithms.cpp algorithms.h

--- src/algorithms.h
+++ src/algorithms.h
@@ -6,12 +6,22 @@
 
 using namespace std;
 
+/**
+ * Conduct Breadth First Search on the graph adjacency matrix 
+ * to find a path between start and end nodes.
+*/
 bool BFS(const int start, const int end, const vector<vector<double>>& matrix);
 
-void printSolution(vector<int> dist);
-
-int minDistance(vector<int> dist, vector<bool> sptSet);
-
-void makePath(const vector<vector<double>>& matrix, const vector<int> parents, const int dest, vector<int>& path);
-
+/**
+ * Conduct Dijkstra's Algorithm to on a graph adjacency matrix
+ * to find the shortest path wetween the source and destination nodes.
+*/
 vector<int> dijkstra(const vector<vector<double>>& matrix, const int source, const int dest);
+
+/**
+ * Helper Function for Dijkstra's Algorithm.
+ * Given the destination node and a parents vector that defines
+ * the parent in the shortest path, populate the path vector with
+ *  the ordered path from source to destination
+*/
+void makePath(const vector<vector<double>>& matrix, const vector<int> parents, const int dest, vector<int>& path);
